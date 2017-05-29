b7459fd0d253ae111c7b9ee0207bfd7495ea5d12
Fabian Valka <fabian.valka@external.t-mobile.at>
2017-05-29 18:53:53 +0200

    Changed back to handling of all files at once

--- src/main/scala/com/vektorraum/aviatorsbot/runway/RunwayImporter.scala
+++ src/main/scala/com/vektorraum/aviatorsbot/runway/RunwayImporter.scala
@@ -2,7 +2,6 @@ package com.vektorraum.aviatorsbot.runway
 
 import java.io.File
 import java.time.LocalDate
-import java.util.concurrent.{ArrayBlockingQueue, ThreadPoolExecutor, TimeUnit}
 
 import com.typesafe.scalalogging.Logger
 import com.vektorraum.aviatorsbot.runway.persistence.{AirfieldsDAO, Db}
@@ -10,11 +9,12 @@ import com.vektorraum.aviatorsbot.runway.persistence.model.{Airfield, Runway}
 import org.orekit.models.earth.GeoMagneticFieldFactory
 import org.xml.sax.SAXParseException
 
-import scala.concurrent.{Await, ExecutionContext}
+import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.io.Source
-import scala.util.{Failure, Success}
+import scala.util.Success
 import scala.xml.{Elem, NodeSeq}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * Created by fvalka on 25.05.2017.
@@ -25,8 +25,6 @@ object RunwayImporter {
 
   import org.orekit.data.DataProvidersManager
   import org.orekit.data.DirectoryCrawler
-
-  import ExecutionContext.Implicits.global
 
   val orekitData = new File("data/")
   val manager: DataProvidersManager = DataProvidersManager.getInstance
@@ -41,41 +39,41 @@ object RunwayImporter {
 
   def main(args: Array[String]): Unit = {
     val fileListPattern = "<a href=\"([A-Za-z]{2}_wpt.aip)\">".r
-    val dirUrl = "http://www.openaip.net/customer_export_sdakjasd1fij1gnfvAFD32f/"
+    val dirUrl = "https://www.openaip.net/customer_export_sdakjasd1fij1gnfvAFD32f/"
     val fileList = Source.fromURL(dirUrl).getLines().mkString
 
 
     logger.info("Converting files")
-    val insertResults = fileListPattern.findAllIn(fileList).matchData flatMap { m =>
+    val airports = fileListPattern.findAllIn(fileList).matchData flatMap { m =>
       val fileName = m.group(1)
       logger.info(s"Downloading and parsing file: $fileName")
       try {
         val file = xml.XML.load(dirUrl + fileName)
         (file \\ "AIRPORT") map { airport =>
-          val airfield = toCaseClass(airport)
-          if (airfield.runways.nonEmpty) {
-            AirfieldsDAO.insertAirfield(airfield) map {
-              _.ok
-            }
-          }
+          toCaseClass(airport)
         }
       } catch {
         case e: SAXParseException => logger.warn(s"Could not parse file $fileName")
           None
       }
-    }
+    } filter { airfield =>
+      airfield.runways.nonEmpty
+    } toList
 
-    val writeErrors = insertResults.count(_ == false)
+    logger.info(s"Found ${airports.size} airfields")
 
-    logger.info(s"Insert completed with $writeErrors write errors")
+    logger.info("Inserting airfields into mongodb")
+    val insertFuture = AirfieldsDAO.insertAirfields(airports)
+    val insertResult = Await.result(insertFuture, 2 minutes)
+    logger.info(s"Insert completed with code: ${insertResult.code} and ${insertResult.writeErrors} write errors")
 
-    if(writeErrors == 0) {
+    if(insertResult.ok) {
       logger.info("Renaming collections to put data live")
       val putAndCloseFuture = Db.putDataLive() andThen {
         case _ => logger.info("Closing database connection")
            Db.close()
       }
-      Await.result(putAndCloseFuture, 180 seconds)
+      Await.result(putAndCloseFuture, 30 seconds)
     }
 
   }
