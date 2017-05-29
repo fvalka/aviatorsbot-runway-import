77f4b40490f3ccc4717978ef037e4304c9618d40
Fabian Valka <fabian.valka@external.t-mobile.at>
2017-05-29 18:11:52 +0200

    Changed execution context

--- src/main/scala/com/vektorraum/aviatorsbot/runway/RunwayImporter.scala
+++ src/main/scala/com/vektorraum/aviatorsbot/runway/RunwayImporter.scala
@@ -2,6 +2,7 @@ package com.vektorraum.aviatorsbot.runway
 
 import java.io.File
 import java.time.LocalDate
+import java.util.concurrent.{ArrayBlockingQueue, ThreadPoolExecutor, TimeUnit}
 
 import com.typesafe.scalalogging.Logger
 import com.vektorraum.aviatorsbot.runway.persistence.{AirfieldsDAO, Db}
@@ -9,12 +10,11 @@ import com.vektorraum.aviatorsbot.runway.persistence.model.{Airfield, Runway}
 import org.orekit.models.earth.GeoMagneticFieldFactory
 import org.xml.sax.SAXParseException
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, ExecutionContext}
 import scala.concurrent.duration._
 import scala.io.Source
 import scala.util.{Failure, Success}
 import scala.xml.{Elem, NodeSeq}
-import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * Created by fvalka on 25.05.2017.
@@ -25,6 +25,21 @@ object RunwayImporter {
 
   import org.orekit.data.DataProvidersManager
   import org.orekit.data.DirectoryCrawler
+
+  val numWorkers = sys.runtime.availableProcessors
+  val queueCapacity = 100
+  implicit val ec = ExecutionContext.fromExecutorService(
+    new ThreadPoolExecutor(
+      numWorkers, numWorkers,
+      0L, TimeUnit.SECONDS,
+      new ArrayBlockingQueue[Runnable](queueCapacity) {
+        override def offer(e: Runnable) = {
+          put(e); // may block if waiting for empty room
+          true
+        }
+      }
+    )
+  )
 
   val orekitData = new File("data/")
   val manager: DataProvidersManager = DataProvidersManager.getInstance
